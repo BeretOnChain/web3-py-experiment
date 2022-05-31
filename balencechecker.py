7f4d4c6c23cf407b76b8a0ec217bfbec0230e742
BeretOnChain <106593391+BeretOnChain@users.noreply.github.com>
2022-05-31 12:37:16 +0100

    Commit #2
    Can now retrieve balances but fails to print the (not a number for conversion to ether from wei)

--- balencechecker.py
+++ balencechecker.py
@@ -3,5 +3,6 @@ infurakey="https://mainnet.infura.io/v3/2070d76c84cd41a58ee6199743878e1c"
 web3 = Web3(Web3.HTTPProvider(infurakey))
 
 address=input("Insert Ethereum address to balence check here : ")
-balence=web3.eth.getBalence(address)
-
+balance=int(web3.eth.getBalance(address))
+ethbalance=balance/10**18
+print("The Balance of this address is "+ethbalance+"ETH") 
